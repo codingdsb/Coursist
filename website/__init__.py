f3cdf93785ac9f278bdcf43f5191df6ec3696b3a
Darshan-Bajeja <codingdsb@gmail.com>
2022-05-16 00:18:28 +0530

    bug fixes

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from os import environ
-
+db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
